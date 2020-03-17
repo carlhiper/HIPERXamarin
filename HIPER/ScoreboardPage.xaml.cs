4a105b79b9c0e5c004590a42ff084ff21594245b
divM AB <divM AB@LAPTOP-ITPMC0H4>
2020-03-17 12:25:16 +0100

    Added switch to show completed and actie goals separate.

--- HIPER/ScoreboardPage.xaml.cs
+++ HIPER/ScoreboardPage.xaml.cs
@@ -27,7 +27,19 @@ namespace HIPER
                 conn.CreateTable<Goal>();
                 var goals = conn.Table<Goal>().ToList();
                 //goalListView.ItemsSource = goals;
+                //Not sure if I will use this -> List<Goal> selectedGoals = new List<Goal>;
                 goalCollectionView.ItemsSource = goals;
+                foreach (var item in goals)
+                {
+                    if (showCompletedSwitch.IsToggled)
+                    {
+                        goalCollectionView.IsVisible = item.completed;
+                    }
+                    else
+                    {
+                        goalCollectionView.IsVisible = !item.completed;
+                    }
+                }
             }
         }
 
