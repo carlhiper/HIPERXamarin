a892fca716397b43693c5b1166bced20baac1664
carlwindfeldt <carl@hiper.se>
2020-03-21 17:49:01 +0100

    Nära en MVP

--- HIPER/FeedPage.xaml.cs
+++ HIPER/FeedPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HIPER.Model;
 using Xamarin.Forms;
 
 namespace HIPER
@@ -10,6 +10,39 @@ namespace HIPER
         public FeedPage()
         {
             InitializeComponent();
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            List<FeedModel> feed = new List<FeedModel>();
+
+
+            // Get users in team
+            var users = await App.client.GetTable<UserModel>().Where(u => u.TeamId == App.loggedInUser.TeamId).ToListAsync();
+           
+       
+            
+            foreach (UserModel user in users)
+            {
+                var goals = await App.client.GetTable<GoalModel>().Where(g => g.UserId == user.Id).ToListAsync();
+
+                foreach (var goal in goals)
+                {
+                    if (!goal.PrivateGoal && !goal.Completed) { 
+                        FeedModel feedItem = new FeedModel();
+                        feedItem.GoalTitle = goal.Title;
+                        feedItem.UserName = user.FirstName + " " + user.LastName;
+                        feedItem.Progress = goal.Progress;
+                        feedItem.Hipes = goal.Hipes;
+                        feed.Add(feedItem);
+                    }
+                }
+            }
+
+            feedCollectionView.ItemsSource = feed;
         }
     }
 }
---
+++ HIPER/Model/FeedModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HIPER.Model
+{
+    public class FeedModel
+    {
+
+        public string UserName { get; set; }
+
+        public string GoalTitle { get; set; }
+    
+        public float Progress { get; set; }
+
+        public int Hipes { get; set; }
+
+        public FeedModel()
+        {
+        }
+    }
+}
