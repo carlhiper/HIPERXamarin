c5ebb7329ac8ef58652c6da73ac971611af1750d
carlwindfeldt <carl@hiper.se>
2020-05-29 13:40:53 +0200

    ...

---
+++ HIPER/ViewModel/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace HIPER.ViewModel.Converters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            DateTimeOffset dateTime = (DateTimeOffset)value;
+            DateTimeOffset rightNow = DateTimeOffset.Now;
+            var differerence = rightNow - dateTime;
+
+            if (differerence.TotalDays >= 7)
+                return $"{dateTime:d}";
+            else
+            {
+                if (differerence.TotalSeconds < 60)
+                    return $"{differerence.TotalSeconds:0} seconds ago";
+                if (differerence.TotalMinutes < 60)
+                    return $"{differerence.TotalMinutes:0} minutes ago";
+                if (differerence.TotalHours < 24)
+                    return $"{differerence.TotalHours:0} hours ago";
+                if (differerence.TotalDays >= 1 && differerence.TotalDays < 2)
+                    return "yesterday";
+
+
+                return $"{differerence.TotalDays:0} days ago";
+
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DateTimeOffset.Now;
+        }
+    }
+}
---
+++ HIPER/ViewModel/Converters/PerformanceToImageConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace HIPER.ViewModel.Converters
+{
+    public class PerformanceToImageConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int indicator = (int)value;
+            if(indicator == 5)
+            {
+                return "arrow_up.png";
+            }else if (indicator == 4)
+            {
+                return "arrow_neutral_up.png";
+            }
+            else if (indicator == 3)
+            {
+                return "arrow_neutral.png";
+            }
+            else if (indicator == 2)
+            {
+                return "arrow_neutral_down.png";
+            }
+            else if (indicator == 1)
+            {
+                return "arrow_down.png";
+            }
+            else { return null; }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
